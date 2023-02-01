c6d70862f03f8ecc86e4f966bd9c1c630586302a
AndreFoulem <timmycasual@icloud.com>
2023-01-31 20:01:13 -0800

    a

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,7 +1,40 @@
-export default function SignUpPage () {
-    return (
-        <div>
-            SignUpForm Here
-        </div>
-    )
+import { Input } from 'react-daisyui'
+import { useForm } from 'react-hook-form'
+import { useCallback } from 'react'
+
+export default function SignUpPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onValid = useCallback(() => {
+    console.log('asdf')
+  }, [second])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexFlow: 'column',
+        gap: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+      onSubmit={handleSubmit(onValid)}
+    >
+      <h2>Sign up form</h2>
+      <div className="form-control w-full max-w-xs">
+        <label htmlFor={'email'} className="lablel">
+          <span className="label-text">email</span>
+        </label>
+
+        <Input color={'ghost'} id={'email'} type={'text'} />
+        <span className="label-text text-error">Error</span>
+      </div>
+    </div>
+  )
 }
